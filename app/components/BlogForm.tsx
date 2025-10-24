067065d6ff6ccef700a5be864020b456a990285a
DerickT02 <derickt18@outlook.com>
2025-10-24 13:10:39 -0700

    downgrade next

--- app/components/BlogForm.tsx
+++ app/components/BlogForm.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
-
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+import ReactQuill from 'react-quill';
 
 export default function BlogForm() {
   const [loading, setLoading] = useState(false);
